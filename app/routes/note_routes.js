c212ec762acb49c2fb3fa745fe5046977411542e
vksantoshsunny <vksantoshsunny@gmail.com>
2017-10-28 11:53:18 +1300

    getall and delete

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -3,6 +3,7 @@ var ObjectID = require('mongodb').ObjectID;
 
 module.exports = function(app, db) {
 
+  //GET
     app.get('/notes/:id', (req, res) => {
         const id = req.params.id;
         const details = { '_id': new ObjectID(id) };
@@ -16,6 +17,25 @@ module.exports = function(app, db) {
         
       });
 
+      //GETALL
+      app.get('/notes', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+        var all = db.collection('notes');
+        
+        all.find({}).toArray(function (err,result) {
+                        if(err){
+                            res.send(err);
+                        }
+                            else{
+        
+                                res.send(JSON.stringify(result));
+                        }
+                    })
+        
+      });
+
+      //POST
     app.post('/notes', (req, res) => {
       const note = { text: req.body.note, title: req.body.value };
       db.collection('notes').insert(note, (err, result) => {
@@ -25,6 +45,19 @@ module.exports = function(app, db) {
           res.send(result.ops[0]);
           console.log('Note Added')
         }
+      });
+    });
+
+    //DELETE
+    app.delete('/notes/:id', (req, res) => {
+      const id = req.params.id;
+      const details = { '_id': new ObjectID(id) };
+      db.collection('notes').remove(details, (err, item) => {
+        if (err) {
+          res.send({'error':'An error has occurred'});
+        } else {
+          res.send('Note ' + id + ' deleted!');
+        } 
       });
     });
   };
